71e28822febb7354f29c08fa1789d5d05514f24d
iamtor9 <iamtor9@gmail.com>
2019-09-12 21:21:11 -0500

    official

--- keys.js
+++ keys.js
@@ -1,9 +1,8 @@
 
-console.log('this is loaded');
+console.log('this is ready');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-// keys
--- liri.js
+++ liri.js
@@ -1,196 +1,158 @@
-// require("dotenv").config();
-
-// const keys = require("./keys.js");
-// const fs = require("fs");
-// const request = require("request");
-// const Spotify = require("node-spotify-api");
-
 require("dotenv").config();
 
-const keys = require("./keys.js");
-
 const fs = require("fs");
-const Moment = require ("moment");
+
+//define letiable requirements for all 
 const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+const moment = require("moment");
 const axios = require("axios");
 
-//takes in user's input and parameters
-const input = process.argv[2];
-const inputParam = "";
-for(const i = 3; i < process.argv.length; i++){
-	inputParam += " " + process.argv[i];
+//create new objects for spotify, movie, and artisteach
+let SPOTIFY = new SPOTIFY();
+let movie = new movie();
+let artist = new artist();
+
+
+let term = process.argV[2]
+let search = process.argV.splice(3).join(" ");
+
+if (!term || !search && term === "movie"){
+    term = "movie"
+    search = "year"
 };
 
-console.log("\n\n\nYEP YUP YUP\n\n")
-
-//spotify api call
-function spotifyIt(){	
-	const spotify = new Spotify(keys.spotify);
-	
-	if(inputParam){
-	spotify.search({type: "track", query: inputParam, limit: 1}, function(err, data){
-		if(err){
-			console.log(err);
-			return;
-		}	
-	
-		for (const i = 0; i < data.tracks.items.length; i++){
-			console.log('SONG NAME', data.tracks.items[i].name);
-			console.log('ARTIST NAME', data.tracks.items[i].artists[0].name);
-			console.log('ALBUM NAME', data.tracks.items[i].album.name);
-			// console.log('PREVIEW LINK', data.tracks.items[i].preview);
-		}	
-  });
-  //TO DO: need to input api key within url
-  } 
-  else if (!inputParam){
-		spotify.request('https://api.spotify.com/v1/tracks/v1/audio-analysis/{id}').then(function(data) {    
-		    console.log("SONG NAME", data.name);
-		    console.log("ARTIST NAME", data.artists[0].name);
-		    console.log("ALBUM NAME", data.album.name);
-		    // console.log("PREVIEW LINK", data.preview_url);   
-		  }).catch(function(err) {
-		    console.error('Error occurred: ' + err); 
-		  });
-	}
+if (!search && term === "concert"){
+    search = "current location"
 };
 
-//omdb api call
-function movie(){	
-	request("http://www.omdbapi.com/?t=" + inputParam + "&y=&plot=short&apikey=trilogy", function(error,response,body){
-		if(!error && response.statusCode===200 && inputParam){			
-			console.log("Movie: " + JSON.parse(body).Title);
-			console.log("Release Year: " + JSON.parse(body).Year);
-			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-			console.log("Rotten Tomatoes Ratings: " + JSON.parse(body).Ratings[1].Value);
-			console.log("Country: " + JSON.parse(body).Country);
-			console.log("Language: " + JSON.parse(body).Language);
-			console.log("Plot: " + JSON.parse(body).Plot);
-			console.log("Actors: " + JSON.parse(body).Actors);
-		} else {
-			request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy", function(error, response, body){
-			console.log("Movie: " + JSON.parse(body).Title);
-			console.log("Release Year: " + JSON.parse(body).Year);
-			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-			console.log("Country: " + JSON.parse(body).Country);
-			console.log("Language: " + JSON.parse(body).Language);
-			console.log("Plot: " + JSON.parse(body).Plot);
-			console.log("Actors: " + JSON.parse(body).Actors);
-			})
-		}
-	})
+if (!search && term === "spotify"){
+    search = "play my jam"
 };
 
-//reads the file function for calling spotifyIt function using contents of the text file as parameter
-function random(){	
-	fs.readFile("random.txt", "utf8", function(error, data){		
-		const newData = data.split(",");		
-		input=newData[0];	
-		inputParam=newData[1];		
-		spotifyIt();
-	})
+if (term === "movie") {
+    movie.findMovie(search);
+}
+else if (term === "concert") {
+    artist.findArtist(search);
+}
+else if (term === "spotify") {
+    SPOTIFY.findSong(search);
+}
+else if (term === "doThis") {
+
+  fs.readFile('random.txt', 'utf8', function(junk, data){
+    let dataArr = data.split('.');
+    console.log(dataArr);
+
+        if (dataArr[0] === "spotify") {
+            SPOTIFY.findSong(dataArr[1]);
+        }
+        else if (dataArr[0] === "concert") {
+            artist.findArtist(dataArr[1]);
+        }
+        else if (dataArr[0] === "movie") {
+            movie.findMovie(dataArr[1]);
+        };
+      })
+
 };
 
-//determine which of the functions to call & return
-if (input==="spotify-this-song"){
-	spotifyIt();	
-} 
-else if (input==="movie-this"){
-	movie();	
-} else if (input==="do-what-it-says"){
-	random();
+// bands in townnnnnnn function and search "this" for bands in town api
+let artist = function() {
+  
+// divide for clarity / artist api search
+// use axios to get api url for data response
+  let divider = "\n------------------------\n\n";
+
+  this.findArtist = function(artist) {
+    let URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    
+    axios.get(URL).then(function(response) {
+      let jsonData = response.data;
+
+// create string for artist's data (use .join)
+    let artistData = [
+        "Venue:" + jsonData[0].venue.name,
+        "Location:" + jsonData[0].venue.country + ", " + jsonData[0].venue.city,
+        "Time:" + moment(jsonData[0].dateTime).format("MM/DD/YYYY"),
+      ].join("\n\n");
+
+    fs.appendFile("log.txt", artistData + divider, function(err) {
+      if (err) throw err;
+      console.log(artistData);
+      });
+    });
+  }};
+
+    module.exports = artist;
+
+// ombd for movies function / find movie via url api
+let movie = function() {
+let divider = "\n--------------------------\n\n";
+
+  this.findMovie = function(movie) {
+    let URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+  axios.get(URL).then(function(response) {
+    let jsonData = response.data;
+
+// create string for movie data (use .join)
+    let movieData = [
+       "Title: " + jsonData.Title,
+       "Year: " + jsonData.Year,
+       "IMDB: " + jsonData.Ratings[0].Value,
+       "Rotten Tomatos: " + jsonData.Ratings[1].Value,
+       "Country: " + jsonData.Country,
+       "Language: " + jsonData.Language,
+       "Plot: " + jsonData.Plot,
+       "Actors: " + jsonData.Actors,
+      ].join("\n\n");
+
+      console.log(movieData);
+
+// append file to text log then console.log moviedata
+  fs.appendFile("log.txt", movieData + divider, function(err) {
+     if (err) throw err;
+      console.log(movieData);
+      });
+    });
+  }};
+
+
+  module.exports = movie;
+
+//spottifyyyyyyyy section
+// Function for running a Spotify search then divider
+let SPOTIFY = function() {
+
+  let divider = "\n---------------------------\n\n";
+
+  this.findSong = function(song) {
+  spotify.search(
+    {
+      type: "track",
+      query: song,
+      limit: 1
+    },
+    function(junk, data) {
+      let songs = data.tracks.items;
+
+      let songData = [
+        "Artist name:" + songs[0].artists[0].name,
+        "Song title:" + songs[0].name,
+        "Album:" + songs[0].album.name,
+        "Preview song:" + songs[0].preview_url,
+      ].join("\n\n");
+
+      console.log(songData);
+  
+      // append file to text log then console.log songdata
+      fs.appendFile("log.txt", songData + divider, function(err) {
+          if (err) throw err;
+        });
+    });
+  }
 };
 
-
-
-  
-// // Create song constructor
-//     const song = function() {
-
-// // have divider that acts as the space between the tv data we print in log.txt
-//     const divider = "\n------------------------------------------------------------\n\n";
-  
-// // findSong takes in name of a song, and searches the spotify API via URL
-
-//         this.findSong = function(song) {
-//             const URL ="https://www.npmjs.com/package/node-spotify-api" + song;
-//         }
-//       }
-
-//     axios.get(URL).then(function(response) {
-//     // Place the response.data into a constiable, jsonData.
-//          const jsonData = response.data;
-  
-// // showSong ends up being the string that contains songs data, then printed to the console
-//      const songData = [
-//         "Song: " + jsonData.name,
-//         "Genre: " + jsonData.genre.join(", "),
-//         "Network: " + jsonData.network.name,
-//         "Summary: " + jsonData.summary
-//         ].join("\n\n");
-  
-//  // Append showData and the divider to log.txt, print showData to the console
-//     fs.appendFile("log.txt", showData + divider, function(err) {
-//       if (err) throw err;
-//       console.log(showData);
-//     });
-
-// // not sure if song/artist/band goes inside this function yet
-//     this.findBands = function(artist) {
-//         const URL = "http://www.artists.bandsintown.com/bandsintown-api" + artist + "/events?app_id=codingbootcamp";
-//     }
-//   })
-// //still need moment.js!!!!!!!!
-
-
-// keep below for my own refrence for learning
-
-// const axios = require("axios");
-//     const fs = require("fs");
-  
-//  Create song constructor
-//       const SONG = function() {
-
-// maybe have divider that acts as the space between the tv data we print in log.txt
-//       const divider = "\n------------------------------------------------------------\n\n";
-  
-// findSong takes in name of a song, and searches the spotify API via URL
-
-//         this.findSong = function(song) {
-//             const URL ="https://www.npmjs.com/package/node-spotify-api" + song;
-//         }
-//       }
-
-//     axios.get(URL).then(function(response) {
-//     // Place the response.data into a constiable, jsonData.
-//          const jsonData = response.data;
-  
-// // showSong ends up being the string that contains songs data, then printed to the console
-//      const songData = [
-//         "Song: " + jsonData.name,
-//         "Genre: " + jsonData.genre.join(", "),
-//         "Network: " + jsonData.network.name,
-//         "Summary: " + jsonData.summary
-//         ].join("\n\n");
-  
-//  // Append showData and the divider to log.txt, print showData to the console
-//     fs.appendFile("log.txt", showData + divider, function(err) {
-//       if (err) throw err;
-//       console.log(showData);
-//     });
-
-// // not sure if song/artist/band goes inside this function yet
-//     this.findBands = function(artist) {
-//         const URL = "http://www.artists.bandsintown.com/bandsintown-api" + artist;
-  
-// //       // Add code to search the TVMaze API for the given actor
-// //       // The API will return an array containing multiple actors, just grab the first result
-// //       // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
-// //       // Print this information to the console
-// //     };
-// //   };
-  
-//      module.exports = Spotify;
-// // // Add code to print whether the user is  searching for an actor or tv show, along with the name of the actor or tv show they are searching for
-
+module.exports = SPOTIFY;
